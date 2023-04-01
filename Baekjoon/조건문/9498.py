301fc600e09248b7b554c26941a6841cf975bc10
toreta3203 <toreta3203@gmail.com>
2023-04-01 22:49:43 +0900

    2023/04/01 Commit-1

---
+++ Baekjoon/조건문/9498.py
@@ -0,0 +1,18 @@
+# 시험 성적
+
+
+A = int(input())
+
+if A >= 90:
+    print("A")
+elif A >= 80:
+    print("B")
+elif A >= 70:
+    print("C")
+elif A >= 60:
+    print("D")
+else:
+    print("F")
+
+# short coding
+# print('FFFFFFDCBAA'[int(input())//10])
